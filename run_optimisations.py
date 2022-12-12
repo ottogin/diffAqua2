970a4d3a78863df45187cba4ec7ef63f24f30f16
Artem Lukoianov <lukartoil@gmail.com>
2022-12-11 20:19:17 -0500

    NEW: running script

---
+++ run_optimisations.py
@@ -0,0 +1,128 @@
+import os
+import torch
+import time
+import json
+import argparse
+
+from src.sdf_convertion import reconstruct_voxels
+from src.simulation import simulate
+
+import sys
+sys.path.insert(1, "external/MeshSDF")
+from lib.models.decoder import DeepSDF
+
+
+DEVICE = "cuda:2"
+
+def run_optimisation(save_path, start_latent_num=14, lr=1e-4, num_iters=100, dx=1./20, dx_sdf=1./32):
+    
+    summary = {
+        "params": {
+            "start_latent_num": start_latent_num,
+            "lr": lr,
+            "num_iters": num_iters
+        },
+        "running_time": time.time(),
+        "run": {
+            "metrics": [],
+            "times": [],
+            "latents": [],
+        }
+    }
+    
+    # Load the model
+    experiment_dir = "runs/wolfish_e256/"
+
+    specs = json.load(open(os.path.join(experiment_dir, "specs.json")))
+    train_mapping = json.load(open(specs["TrainSplit"]))
+    data_mapping = json.load(open("/".join(specs["TrainSplit"].split("/")[:-1] + ["mapping.json"])))
+
+    # Load the model
+    decoder = torch.nn.DataParallel(DeepSDF(specs["CodeLength"],  **specs["NetworkSpecs"]), device_ids=[DEVICE])
+    saved_model_state = torch.load(
+        os.path.join(experiment_dir, "ModelParameters", "latest.pth"), map_location=DEVICE
+    )
+    decoder.load_state_dict(saved_model_state["model_state_dict"])
+    decoder = decoder.eval()
+
+    # Load latent codes
+    orig_latents = torch.load(os.path.join(experiment_dir, "LatentCodes/latest.pth"), map_location=DEVICE)["latent_codes"]["weight"]
+        
+    latent_codes = []
+    metric  = []
+    latent = torch.clone(orig_latents[start_latent_num]).requires_grad_(True)
+    optimizer = torch.optim.Adam([latent]) #3e-4
+
+    latent_codes.append(latent.clone())
+
+    start_time = time.time()
+    for it in range(num_iters):
+        optimizer.zero_grad()
+
+        # Forward
+        voxels = reconstruct_voxels(decoder, latent, N=[64, 32, 32])
+        speed, voxel_mesh = simulate(voxels)
+        loss = -speed
+        loss.backward()
+
+        # Backward through MeshSDF - don't forget to convert the 
+        # Modify the dL/dx_i to be in the DeepSDF coordinate system
+        dL_dx_i = dx / dx_sdf * voxel_mesh.grad.reshape(-1, 3).type(torch.FloatTensor)
+        dL_dx_i = dL_dx_i.to(DEVICE)
+        # use vertices to compute full backward pass
+        optimizer.zero_grad()
+
+        # Convert voxel_mesh to DeepSDF cooridnate system !!!!!!!!
+        voxel_mesh = voxel_mesh.reshape(-1, 3) * dx_sdf / dx + torch.tensor([[-1, -0.5, -0.5]], dtype=voxel_mesh.dtype, device=voxel_mesh.device)
+        xyz = voxel_mesh.clone().detach().type(torch.FloatTensor)
+        xyz = xyz.to(DEVICE).requires_grad_(True)
+        latent_inputs = latent.expand(xyz.shape[0], -1)
+
+        #first compute normals
+        pred_sdf = decoder(latent_inputs, xyz)
+        # for df, grad in zip(pred_sdf, dL_dx_i):
+        #     print(df.item(), grad.item())
+
+        loss_normals = torch.sum(pred_sdf)
+        loss_normals.backward(retain_graph = True)
+        # normalization to take into account for the fact sdf is not perfect...
+
+        normals = xyz.grad/torch.norm(xyz.grad, 2, 1).unsqueeze(-1)
+        # now assemble inflow derivative
+        optimizer.zero_grad()
+        
+        # Drop points inside the mesh - gradients there are not reliable
+        filt = (pred_sdf[:, 0] <= dx_sdf / 2) & (pred_sdf[:, 0] >= -dx_sdf / 2)
+        dL_ds_i = -torch.matmul(dL_dx_i[filt].unsqueeze(1), normals[filt].unsqueeze(-1)).squeeze(-1)
+        # refer to Equation (4) in the main paper
+        loss_backward = torch.sum(dL_ds_i * pred_sdf[filt])
+        loss_backward.backward()
+        # and update params
+        optimizer.step()
+
+        time_elapsed = time.time() - start_time
+
+        summary["run"]["times"].append(time_elapsed)
+        summary["run"]["metrics"].append(speed.item())
+        summary["run"]["latents"].append(latent.cpu().detach().numpy().tolist())
+        print(f"#{it:4d} : Fish Speed: {speed:.5f}       Time Elapsed: {time_elapsed:.2f}", end='\r')
+        
+        with(open(save_path, "w")) as f:
+            json.dump(summary, f)
+
+    return summary
+
+
+if __name__ == "__main__":    
+    base_dir = "optimisation_runs"
+    
+    parser = argparse.ArgumentParser(prog = 'Fish Optimiser', description = 'Optimise fish perfomance with DeepSDF')
+    parser.add_argument('name', action='store', type=str, help='The text to parse.')
+    parser.add_argument('--lr', default=1e-4, type=float)
+    parser.add_argument('--nits', default=100, type=int)
+    parser.add_argument('--startshape', default=14, type=int)
+
+    args = parser.parse_args()
+
+    print(f"Start optimisation {args.name} from shape #{args.startshape} with lr={args.lr} and {args.nits} iterations")
+    run_optimisation(os.path.join(base_dir, args.name + ".json"), start_latent_num=args.startshape, lr=args.lr, num_iters=args.nits)
---
+++ src/simulation.py
@@ -0,0 +1,138 @@
+from pathlib import Path
+import sys
+import time
+import math
+import random
+import copy
+from collections import deque
+from tqdm import trange, tqdm
+
+import scipy
+import scipy.optimize
+import numpy as np
+
+import torch
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+
+import trimesh
+import pyvista as pv
+import imageio
+
+from diffpd.fem import DeformableHex, HydrodynamicsStateForceHex
+from diffpd.sim import Sim
+from diffpd.nn import OpenFoldController
+from diffpd import transforms
+from diffpd.mesh import MeshHex
+
+seed = 42
+pv.start_xvfb()
+
+# Mesh parameters.
+length = 20
+dx = 1. / length
+
+# Hydrodynamics parameters.
+rho = 1e1
+v_water = [0, 0, 0]   # Velocity of the water.
+# Cd_points = (angle, coeff) pairs where angle is normalized to [0, 1].
+Cd_points = np.array([
+    [0.0, 0.05],
+    [0.4, 0.05],
+    [0.7, 1.85],
+    [1.0, 2.05],
+]) # * 1.0
+# Ct_points = (angle, coeff) pairs where angle is normalized to [-1, 1].
+Ct_points = np.array([
+    [-1, -0.8],
+    [-0.3, -0.5],
+    [0.3, 0.1],
+    [1, 2.5],
+])
+
+# FEM parameters.
+youngs_modulus = 1e6
+poissons_ratio = 0.45
+average_density = 1e1
+dt = 3.33e-3
+method = 'pd_eigen'
+options = {
+    'max_pd_iter': 2000, 'max_ls_iter': 10, 'abs_tol': 1e-4, 'rel_tol': 1e-3,
+    'verbose': 0, 'thread_ct': 64, 'use_bfgs': 1, 'bfgs_history_size': 10
+}
+
+
+def simulate(voxels, num_frames=20):
+    ## Inint simulator
+
+    shape = voxels.shape
+    rest_mesh = MeshHex.load(voxels.clone().detach().numpy(), dx=dx)
+    transform = []
+    transform.append(transforms.AddStateForce(
+        'hydrodynamics', [rho] + v_water + Cd_points.ravel().tolist() + Ct_points.ravel().tolist() + rest_mesh.boundary.ravel().tolist()))
+
+    actuator_scale = 0.04
+    actuator_height = int(shape[2] * actuator_scale)
+    actuator_width =  int(shape[1] * actuator_scale)
+
+    all_muscles = []
+    shared_muscles = []
+    for z in range(int(shape[2] / 2) - actuator_height, int(shape[2] / 2) + actuator_height):
+        muscle_pair = []
+        for y in range(int(shape[1] / 2) - actuator_width, int(shape[1] / 2) + actuator_width):
+            indices = rest_mesh.cell_indices[int(0.45 * shape[0]):int(0.5 * shape[0]), y, z].tolist()
+            transform.append(transforms.AddActuationEnergy(1e6, [1.0, 0.0, 0.0], indices))
+            muscle_pair.append(indices)
+
+            # print(indices)
+        shared_muscles.append(muscle_pair)
+    all_muscles.append(shared_muscles)
+
+    transform = transforms.Compose(transform)
+
+    deformable = DeformableHex(
+        rest_mesh, density=average_density, dt=dt, method=method, options=options,)
+    deformable = transform(deformable)
+
+    dofs = deformable.dofs()
+    act_dofs = deformable.act_dofs()
+
+    q0 = torch.as_tensor(rest_mesh.vertices).view(-1).clone().detach().to(torch.float64)
+    v0 = torch.zeros(dofs).detach().to(torch.float64)
+
+    head_indices = rest_mesh.node_indices[0, 0, 0]
+
+    sim = Sim(deformable)
+    sim.add_default_pd_energies(['corotated'], youngs_modulus, poissons_ratio)
+
+    voxel_mesh = q0.clone()
+    voxel_mesh.requires_grad = True
+    voxel_mesh = voxel_mesh.to(torch.float64)
+
+    controller = OpenFoldController(
+        deformable, all_muscles,
+        num_steps=num_frames,
+        segment_len=1,
+        init_period=16.0,
+        init_magnitude=128.0).to(torch.float64)
+
+    a = None
+    q, v = q0, v0
+
+    qs, vs = [], []
+    for a in controller():
+        q, v = sim(q, v, a, shape=voxel_mesh)
+
+        qs.append(q)
+        vs.append(v)
+
+    # for idx in range(20):
+    #     print(torch.mean(vs[idx].reshape(-1, 3)[:, 0]).item(),
+    #           torch.mean(vs[idx].reshape(-1, 3)[:, 1]).item(),
+    #           torch.mean(vs[idx].reshape(-1, 3)[:, 2]).item()
+    #         )
+    speed = torch.mean((qs[-1] - q0).reshape(-1, 3)[:, 0])#torch.mean(torch.concatenate(vs).reshape(-1, 3))
+    
+    return speed, voxel_mesh
+        
